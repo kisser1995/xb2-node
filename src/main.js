7021282d5033e9cd0e4b6479ee24b457ddc8a2fb
Henry <32307786@qq.com>
2020-06-25 22:20:58 +0800

    express

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
-const http=require("http");
+const express= require('express');
+const app=express();
+const port=3000;
 
-const server = http.createServer((request,response)=>{
-    response.write('hello~');
-    response.end();
+app.listen(3000,()=>{
+    console.log('服务已启动')
 })
 
-server.listen(3000,()=>{
-    console.log('服务已启动');
+const data=[
+    {
+        id:1,
+        title:'望月-1'
+    },
+    {
+        id:2,
+        title:'望月-2'
+    },
+    {
+        id:3,
+        title:'望月-3'
+    }
+];
+
+app.get("/post",(req,res)=>{
+    res.send(data)
+})
+
+app.get('/post/:postId',(req,res)=>{
+    const {postId} =req.params;
+    const post=data.filter(item=>item.id==postId)
+    res.send(post[0])
 })
